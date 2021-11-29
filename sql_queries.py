11084480d94a816a888d9bc6aba9fbcaa9fa9b21
Aléaume <aliaume64@gmail.com>
2021-11-29 21:39:48 +0100

    All tested and running // before 1st submit

--- sql_queries.py
+++ sql_queries.py
@@ -22,22 +22,22 @@ time_table_drop = "DROP table IF EXISTS time"
 staging_events_table_create= ("""CREATE TABLE IF NOT EXISTS staging_events(\
                                 artist varchar,
                                 auth varchar,
-                                firstname varchar,
+                                firstName varchar,
                                 gender varchar,
-                                item_in_session int,
-                                lastname varchar,
+                                itemInSession int,
+                                lastName varchar,
                                 length float,
                                 level varchar,
                                 location varchar,
                                 method varchar,
                                 page varchar,
                                 registration float,
-                                session_id int,
+                                sessionId int,
                                 song varchar,
                                 status int,
-                                timestamp timestamp,
-                                user_agent varchar,
-                                user_id int
+                                ts bigint,
+                                userAgent varchar,
+                                userId int
     )
 
 """)
@@ -106,11 +106,12 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time (\
 # STAGING TABLES
 
 staging_events_copy = ("""
-copy staging_events from 's3://udacity-dend/log_data/{}' 
+copy staging_events from 's3://udacity-dend/log_data/' 
 credentials 'aws_iam_role={}'
-format as json 'auto'
-region 'us-west-2';
-""").format('', ARN)
+format json as 's3://udacity-dend/log_json_path.json'
+region 'us-west-2'
+dateformat 'auto';
+""").format(ARN)
 
 
 staging_songs_copy = ("""
@@ -123,43 +124,61 @@ region 'us-west-2';
 # FINAL TABLES
 
 songplay_table_insert = ("""INSERT INTO songplays(start_time, user_id,level, song_id, artist_id, session_id, location, user_agent)\
-                        SELECT timestamp, user_id, level, xxx \
-                        FROM 
+                                SELECT DISTINCT timestamp 'epoch' + CAST(e.ts AS BIGINT)/1000 * interval '1 second' ,\
+                                        e.userId, e.level, s.song_id, s.artist_id, e.sessionId, e.location, e.userAgent \
+                                FROM staging_events e \
+                                INNER JOIN staging_songs s \
+                                ON e.song = s.title AND \
+                                e.artist = s.artist_name AND \
+                                e.length = s.duration \
+                                WHERE e.page = 'NextSong' \
+                                AND userID IS NOT NULL;
                        
                         
 """)
 
 user_table_insert = ("""INSERT INTO users(user_id, first_name, last_name, gender, level)\
-                    SELECT user_id, firstname, lastname, gender, level FROM staging_events \
-                    ON CONFLICT (user_id) \
-                    DO UPDATE SET \
-                    level = EXCLUDED.level,\
-                    last_name = EXCLUDED.level,\
-                    gender = EXCLUDED.gender;
+                            SELECT DISTINCT userId, firstName, lastName, gender, level \
+                            FROM staging_events \
+                            WHERE page = 'NextSong'\
+                            AND userID IS NOT NULL;
+                       
 """)
 
 song_table_insert = ("""INSERT INTO songs(song_id, title, artist_id, year, duration)\
-                    SELECT song_id, title, artist_id, year, duration FROM staging_songs \
-                    ON CONFLICT (song_id) \
-                    DO NOTHING;
+                            SELECT DISTINCT song_id, title, artist_id, year, duration \
+                            FROM staging_songs;
+                   
 """)
 
 artist_table_insert = ("""INSERT INTO artists(artist_id, name, location, latitude, longitude)\
-                        SELECT artist_id, artist_name, artist_location, artist_latitude, artist_longitude FROM staging_songs \
-                        ON CONFLICT (artist_id) \
-                        DO UPDATE SET \
-                        name = EXCLUDED.name,\
-                        location = EXCLUDED.location, \
-                        latitude = EXCLUDED.latitude,\
-                        longitude = EXCLUDED.longitude;
+                            SELECT DISTINCT artist_id, artist_name, artist_location, artist_latitude, artist_longitude \
+                            FROM staging_songs;
+                        
 """)
 
 
 time_table_insert = ("""INSERT INTO time(start_time, hour, day, week, month, year, weekday)\
-                    SELECT timestamp, EXTRACT(HOUR FROM timestamp), EXTRACT(DAY FROM timestamp), EXTRACT(WEEK FROM timestamp), EXTRACT(MONTH FROM timestamp), EXTRACT(YEAR FROM timestamp), EXTRACT(WEEKDAY FROM timestamp)  FROM staging_events \
-                    ON CONFLICT (start_time) \
-                    DO NOTHING;
+                            SELECT DISTINCT timestamp 'epoch' + CAST(ts AS BIGINT)/1000 * interval '1 second' AS ts_ts, \
+                            EXTRACT(HOUR FROM ts_ts), \
+                            EXTRACT(DAY FROM ts_ts), \
+                            EXTRACT(WEEK FROM ts_ts),\
+                            EXTRACT(MONTH FROM ts_ts),\
+                            EXTRACT(YEAR FROM ts_ts), \
+                            EXTRACT(WEEKDAY FROM ts_ts)  \
+                            FROM staging_events \
+                            WHERE page = 'NextSong';
 """)
+
+sql_count_songplays = "SELECT COUNT(*) FROM songplays;"
+sql_count_songs = "SELECT COUNT(*) FROM songs"
+sql_count_artists = "SELECT COUNT(*) FROM artists;"
+sql_count_time = "SELECT COUNT(*) FROM time;"
+sql_count_users = "SELECT COUNT(*) FROM users;"
+sql_count_staging_events = "SELECT COUNT(*) FROM staging_events;"
+sql_count_staging_songs = "SELECT COUNT(*) FROM staging_songs;"
+
+#SOURCE / HELP : for timestamp conversion, https://stackoverflow.com/questions/39815425/how-to-convert-epoch-to-datetime-redshift + discussions in udacity forum 
 
 # QUERY LISTS
 
@@ -167,3 +186,4 @@ create_table_queries = [staging_events_table_create, staging_songs_table_create,
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+sql_counts = [sql_count_songplays, sql_count_songs, sql_count_artists, sql_count_time, sql_count_users, sql_count_staging_events, sql_count_staging_songs]
