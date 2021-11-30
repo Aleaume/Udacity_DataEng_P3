440efc3aceab191c71f01a9c3d9f9ecead13737c
Aléaume <32632731+Aleaume@users.noreply.github.com>
2021-11-30 11:25:08 +0100

    Update etl.py

--- etl.py
+++ etl.py
@@ -4,18 +4,55 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    
+     """
+    Description: This function is responsible for reading the json file in the s3 bucket, and copyting the data in the staging tables
+    
+    Arguments:
+        cur: the cursor object.
+        filepath: song data file path.
+        
+    Returns:
+        None
+    """
+    
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    
+    """
+    Description: This function is insert data into the final tables from the staging tables.
+    
+    Arguments:
+        cur: the cursor object.
+        filepath: song data file path.
+        
+    Returns:
+        None
+    """
+    
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    
+     """
+    Description: This function is the main function of this file.
+                 It creates the connection to the DB and its cursor.
+                 Then calls the respective etl functions to
+                 execute the complete etl pipeline.
+    Arguments:
+        None
+    Returns:
+        None
+    """
+    
+    
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
