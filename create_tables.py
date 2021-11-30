647230e331d11b2305985cdf2463bdcbd6590634
Aléaume <32632731+Aleaume@users.noreply.github.com>
2021-11-30 11:21:47 +0100

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,67 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    
+     """
+    Description: Drops each table using the queries in `drop_table_queries` list.
+    
+    Arguments: 
+       cur: the cursor object
+       conn: connection to the DB
+    
+    Returns:
+        None
+        
+    
+    """
+    
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    
+    """
+        Description: Creates each table using the queries in `create_table_queries` list.
+        
+        Arguments: 
+           cur: the cursor object
+           conn: connection to the DB
+    
+        Returns:
+            None
+    """
+    
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    
+    """
+    Description: 
+        - Drops (if exists) and Creates the sparkify database. 
+    
+        - Establishes connection with the sparkify database and gets
+          cursor to it.  
+    
+        - Drops all the tables.  
+    
+        - Creates all tables needed. 
+    
+        - Finally, closes the connection.
+    
+    Arguments: 
+       None
+    
+    Returns:
+       None
+        
+    """
+    
+    
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
